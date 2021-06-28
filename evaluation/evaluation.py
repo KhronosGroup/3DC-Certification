d6a16ae42417b35d081bbebc86db5cbd490d63de
Moritz Willy Becher <becher@ux3d.io>
2021-06-28 17:07:04 +0200

    clean up script

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -3,50 +3,99 @@
 import argparse
 from pathlib import Path
 import os
-from skimage import io
-from skimage import metrics
-from skimage import color
-from skimage.util import img_as_float
+import skimage.io
+import skimage.metrics
+import skimage.color
+import skimage.util
 import numpy as np
+import itertools
+from sewar.full_ref import vifp, uqi
 
-from sewar.full_ref import vifp
+def gather_image_pairs(ref_path: Path, cert_path: Path):
+    reference_paths = sorted(list((ref_path / "models").glob("*/rr-*.png")))
+    certification_candidate_paths = list(cert_path.glob("c-*.png"))
+    return [ 
+        (r, c) 
+        for r, c in itertools.product(reference_paths, certification_candidate_paths) 
+        if r.name.replace("rr-", "c-", 1) == c.name
+    ]
 
-parser = argparse.ArgumentParser(description='Generate screenshots for certification')
-parser.add_argument("--rep", help="Path to the certification repository", default="..")
-parser.add_argument("--dir", help="Path to folder with the generated screenshots")
-args = parser.parse_args()
+def evaluate_metrics(reference, candidate):
+    return {
+        # Structural similarity index between two images
+        # (humans are better at perceiving structural differences than subtle pixel changes).
+        # it compares the images based on luminance Contast and structure.
+        # ssim provides much better results when applied to small patches of the image than on its entirety. Typically an 11x11 window is used.
+        # ssim provides scores from -1 (not similar) to 1 (same image).
+        # ssim provides high scores one of the images is blured or the color space is shifted
+        # further reading: https://medium.com/srm-mic/all-about-structural-similarity-index-ssim-theory-code-in-pytorch-6551b455541e
+        # https://en.wikipedia.org/wiki/Structural_similarity
+        "ssim": skimage.metrics.structural_similarity(reference, candidate, multichannel=True),
+        # Mean Squared Error (MSE) is one of the most commonly used image quality measures, but receives strong criticism as
+        # it doesn't reflect the way the human visual systems perceive images very well. An image pair with
+        # high MSE might still look very similar to a human.
+        "mse": skimage.metrics.mean_squared_error(reference, candidate),
+        # Peak Signal to Noise Ratio (PSNR) is based on MSE and brought to a logarithmic scale in the decibel unit
+        "psnr": skimage.metrics.peak_signal_noise_ratio(reference, candidate),
+        # Normalized Root MSE (NRMSE)
+        "nrmse": skimage.metrics.normalized_root_mse(reference, candidate),
+        # Mean Absolute Error (MAE)
+        "mae": np.absolute(np.subtract(reference, candidate)).mean(),
+        # Visual Information Fidelity Measure (VIF)
+        # https://ieeexplore.ieee.org/abstract/document/1576816/
+        # The higher the better. The reference image would yield a value of 1.0, values above 1.0
+        # typically stem from higher contrast images, which are considerered better quality in this metric
+        "vif": vifp(reference, candidate),
+        # Universal Quality Index (UQI)
+        "uqi": uqi(reference, candidate),
+    }
+    
+def evaluate_passed(metrics):
+    # TODO: the values here are just suggestions, we have to evaluate which values
+    # are strict enough to ensure visual similarity but not be too strict at the same time
+    return {
+        # Choose a relaxed value for SSIM
+        "ssim": metrics["ssim"] > 0.95,
+        # PSNR for image compression in 8bit is typically in the range [30, 50]
+        "psnr": metrics["psnr"] > 35.0,
+    }
 
-cert_path = Path(args.rep)
-screenshots_dir = Path(args.dir)
+def print_report(metrics_report):
+    print(metrics_report['name'])
 
-dirnames = next(os.walk(cert_path / "models"))[1]
-for dir in dirnames:
-    filenames = next(os.walk(cert_path / "models" / dir))[2]
-    for filename in filenames:
-        if filename.startswith("rr-"):
-            print("Compare: " + filename)
-            im1 = img_as_float(io.imread(cert_path / "models" / dir / filename))
-            screenshotsName = "c" + filename.lstrip('r')
-            im2 = img_as_float(io.imread(screenshots_dir / screenshotsName))
-            if im2.shape[2] == 3:
-                im1 = color.rgba2rgb(im1)
-            print("SSIM:  {:.5f}".format(metrics.structural_similarity(im1, im2, multichannel=True)))
-            print("MSE:   {:.5f}".format(metrics.mean_squared_error(im1, im2)))
-            print("NRMSE: {:.5f}".format(metrics.normalized_root_mse(im1, im2)))
-            print("MAE:   {:.5f}".format(np.absolute(np.subtract(im1, im2)).mean()))
-            print("PSNR:  {:.5f}".format(metrics.peak_signal_noise_ratio(im1, im2)))
-            print("VIF:   {:.5f}".format(vifp(im1, im2)))
+    for name, value in metrics_report["metrics"].items():
+        passed = ''
+        if name in metrics_report['passed']:
+            passed = f"[{'Passed' if metrics_report['passed'][name] else 'Failed'}]"
+        print(f"{name: <8} {value :<12.6f} {passed}")
 
+    print("")
 
-# ssim: this metric compares structural similarity between images (humans are better at perceiving structural differences than subtle pixel changes).
-# it compares the images based on luminance Contast and structure.
-# ssim provides much better results when applied to small patches of the image than on its entirety. Typically an 11x11 window is used.
-# ssim provides scores from -1 (not similar) to 1 (same image).
-# ssim provides high scores one of the images is blured or the color space is shifted
-# further reading: https://medium.com/srm-mic/all-about-structural-similarity-index-ssim-theory-code-in-pytorch-6551b455541e
-# https://en.wikipedia.org/wiki/Structural_similarity
-# personally i'd say everything above 0.96 is "close enough".
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate screenshots for certification')
+    parser.add_argument("--rep", help="Path to the certification repository", default="..")
+    parser.add_argument("--dir", help="Path to folder with the generated screenshots")
+    args = parser.parse_args()
 
-# to my knowledge the best image comparison metric based on human judgement
-# is based on neural networks and can be found in this paper https://arxiv.org/abs/1801.03924
-# github: https://github.com/richzhang/PerceptualSimilarity
+    cert_path = Path(args.rep)
+    screenshots_dir = Path(args.dir)
+    
+    image_pairs = gather_image_pairs(cert_path, screenshots_dir)
+    for reference_path, candidate_path in image_pairs:
+        
+        im1 = skimage.util.img_as_float(skimage.io.imread(reference_path))
+        im2 = skimage.util.img_as_float(skimage.io.imread(candidate_path))
+        if im2.shape[2] == 3:
+            im1 = skimage.color.rgba2rgb(im1)
+
+        name = reference_path.name.replace("rr-", "", 1).replace(".png", "")
+        metrics = evaluate_metrics(im1, im2)
+        passed = evaluate_passed(metrics)
+
+        report = {
+            "name": name,
+            "metrics": metrics,
+            "passed": passed
+        }
+        
+        print_report(report)
