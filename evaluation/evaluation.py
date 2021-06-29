3b492e77a5d62d8ae56e990f6407fb403b89a928
Moritz Willy Becher <becher@ux3d.io>
2021-06-29 17:14:53 +0200

    generate evaluation report pdf

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -10,6 +10,7 @@ import skimage.util
 import numpy as np
 import itertools
 from sewar.full_ref import vifp, uqi
+import report
 
 def gather_image_pairs(ref_path: Path, cert_path: Path):
     reference_paths = sorted(list((ref_path / "models").glob("*/rr-*.png")))
@@ -34,20 +35,20 @@ def evaluate_metrics(reference, candidate):
         # Mean Squared Error (MSE) is one of the most commonly used image quality measures, but receives strong criticism as
         # it doesn't reflect the way the human visual systems perceive images very well. An image pair with
         # high MSE might still look very similar to a human.
-        "mse": skimage.metrics.mean_squared_error(reference, candidate),
+        # "mse": skimage.metrics.mean_squared_error(reference, candidate),
         # Peak Signal to Noise Ratio (PSNR) is based on MSE and brought to a logarithmic scale in the decibel unit
         "psnr": skimage.metrics.peak_signal_noise_ratio(reference, candidate),
         # Normalized Root MSE (NRMSE)
-        "nrmse": skimage.metrics.normalized_root_mse(reference, candidate),
+        # "nrmse": skimage.metrics.normalized_root_mse(reference, candidate),
         # Mean Absolute Error (MAE)
-        "mae": np.absolute(np.subtract(reference, candidate)).mean(),
+        # "mae": np.absolute(np.subtract(reference, candidate)).mean(),
         # Visual Information Fidelity Measure (VIF)
         # https://ieeexplore.ieee.org/abstract/document/1576816/
         # The higher the better. The reference image would yield a value of 1.0, values above 1.0
         # typically stem from higher contrast images, which are considerered better quality in this metric
         # "vif": vifp(reference, candidate),
         # Universal Quality Index (UQI)
-        "uqi": uqi(reference, candidate),
+        # "uqi": uqi(reference, candidate),
     }
     
 def evaluate_passed(metrics):
@@ -60,8 +61,8 @@ def evaluate_passed(metrics):
         "psnr": metrics["psnr"] > 30.0, # maybe 32
     }
 
-def print_report(metrics_report):
-    print(metrics_report['name'])
+def print_report(name, metrics_report):
+    print(name)
 
     for name, value in metrics_report["metrics"].items():
         passed = ''
@@ -72,16 +73,20 @@ def print_report(metrics_report):
     print("")
 
 def compare_images(reference, candidate):
+
+    if reference.shape[2] == 4:
+        reference = skimage.color.rgba2rgb(reference)
+    if candidate.shape[2] == 4:
+        candidate = skimage.color.rgba2rgb(candidate)
     diff = skimage.util.compare_images(reference, candidate, method='diff')
-    if diff.shape[2] == 4:
-        diff.putalpha(1.0)
+
     return {
         "reference": reference,
         "candidate": candidate,
         "diff": skimage.util.img_as_ubyte(diff),
     }
 
-def evaluate(name, reference, candidate):
+def evaluate(reference, candidate):
     if candidate.shape[2] == 3:
         reference = skimage.util.img_as_ubyte(skimage.color.rgba2rgb(reference))
 
@@ -92,37 +97,45 @@ def evaluate(name, reference, candidate):
     metrics = evaluate_metrics(reference, candidate)
 
     return {
-        "name": name,
         "metrics": metrics,
         "passed": evaluate_passed(metrics),
         "images": compare_images(reference, candidate),
     }
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate screenshots for certification')
-    parser.add_argument("--rep", help="Path to the certification repository", default="..")
-    parser.add_argument("--dir", help="Path to folder with the generated screenshots")
-    parser.add_argument("--plot_dir", help="Path to which to store comparison plots")
+    parser.add_argument("--rep", "-r", help="Path to the certification repository", default="..")
+    parser.add_argument("--dir", "-d", help="Path to folder with the generated screenshots")
+    parser.add_argument("--name", "-n", required=True, help="Name of the certification submission")
+    parser.add_argument("--output", "-o", help="Output directory for results")
     args = parser.parse_args()
 
     cert_path = Path(args.rep)
     screenshots_dir = Path(args.dir)
-    diff_path = None
-    if args.plot_dir:
-        diff_path = Path(args.plot_dir)
-        os.makedirs(diff_path, exist_ok=True)
+    output_path = None
+    if args.output:
+        output_path = Path(args.output)
+        os.makedirs(output_path, exist_ok=True)
+        os.makedirs(output_path / "diffs", exist_ok=True)
     
     image_pairs = gather_image_pairs(cert_path, screenshots_dir)
+
+    results = {}
     for reference_path, candidate_path in image_pairs:
         im1 = skimage.io.imread(reference_path)
         im2 = skimage.util.img_as_ubyte(skimage.io.imread(candidate_path))
         name = reference_path.name.replace("rr-", "", 1).replace(".png", "")
 
-        report = evaluate(name, im1, im2)
+        results[name] = evaluate(im1, im2)
         
-        print_report(report)
-        if diff_path:
-            skimage.io.imsave(diff_path / f"rr-{name}.png", report["images"]["reference"])
-            skimage.io.imsave(diff_path / f"c-{name}.png", report["images"]["candidate"])
-            skimage.io.imsave(diff_path / f"d-{name}.png", report["images"]["diff"], check_contrast=False)
+        print_report(name, results[name])
+        if output_path:
+            diff_image_path = output_path / "diffs" / f"d-{name}.png"
+            # skimage.io.imsave(output_path / f"rr-{name}.png", results[name]["images"]["reference"])
+            # skimage.io.imsave(output_path / f"c-{name}.png", results[name]["images"]["candidate"])
+            skimage.io.imsave(diff_image_path, results[name]["images"]["diff"], check_contrast=False)
+            results[name]["images"]["reference_path"] = reference_path
+            results[name]["images"]["candidate_path"] = candidate_path
+            results[name]["images"]["diff_path"] = diff_image_path
+
+    report.generate_report_document(results, output_path / "report.pdf", args.name)
---
+++ evaluation/report.py
@@ -0,0 +1,76 @@
+import time
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+
+def generate_report_document(report_data, path, name):
+    doc = SimpleDocTemplate(str(path.absolute()),
+                        rightMargin=18,leftMargin=18,
+                        topMargin=72,bottomMargin=18)
+    
+    stylesheet = getSampleStyleSheet()
+
+    story=[]
+
+    story.append(Paragraph(f"{name} - Certification Report",  stylesheet["Heading1"]))
+
+    story.append(Paragraph(f"{time.ctime()}"))
+
+    story.append(Spacer(1, 12))
+
+    # List each test case with results
+    for name, result in report_data.items():
+        story.append(Paragraph(name, stylesheet["Heading2"]))
+
+        # Evaluated metrics
+        metrics_data = [
+            [
+                Paragraph("Metric", stylesheet["Heading4"]),
+                Paragraph("Value", stylesheet["Heading4"]),
+                Paragraph("Result", stylesheet["Heading4"])
+            ],
+            [
+                Paragraph("SSIM"), 
+                Paragraph(f'<code>{result["metrics"]["ssim"]:10.5f}</code>'), 
+                Paragraph("Passed" if result["passed"]["ssim"] else '<font color="red">Failed</font>')
+            ],
+            [
+                Paragraph("PSNR"), 
+                Paragraph(f'<code>{result["metrics"]["psnr"]:10.5f}</code>'), 
+                Paragraph("Passed" if result["passed"]["psnr"] else '<font color="red">Failed</font>')
+            ],
+        ]
+        t = Table(metrics_data, 3 * [2.5 * inch])
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+        ]))
+        story.append(t)
+
+        story.append(Spacer(1, 12))
+
+        images_data = [
+            [ 
+                Paragraph("Reference", stylesheet["Heading4"]), 
+                Paragraph("Candidate", stylesheet["Heading4"]), 
+                Paragraph("Difference", stylesheet["Heading4"])
+            ],
+            [
+                Image(result["images"]["reference_path"], width=2*inch, height=2*inch),
+                Image(result["images"]["candidate_path"], width=2*inch, height=2*inch),
+                Image(result["images"]["diff_path"], width=2*inch, height=2*inch),
+            ]
+        ]
+        t = Table(images_data, 3 * [2.5 * inch])
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+        ]))
+        story.append(t)
+
+        story.append(Spacer(1, 12))
+        
+
+    doc.build(story)
